3303883060613402ae2815f1c48a6071b846ac98
fwdAndroid <fwdkaleem@gmail.com>
2024-10-01 04:22:50 +0500

    Clinic

--- lib/widgets/header_widget.dart
+++ lib/widgets/header_widget.dart
@@ -1,5 +1,12 @@
 import 'package:estheva_web/uitls/colors.dart';
 import 'package:estheva_web/website/screens/appointment_web/appointment_page_web.dart';
+import 'package:estheva_web/website/screens/clinic_services/aestheticatclince.dart';
+import 'package:estheva_web/website/screens/clinic_services/bodu_contouring_page_clinic.dart';
+import 'package:estheva_web/website/screens/clinic_services/hair_transplant.dart';
+import 'package:estheva_web/website/screens/clinic_services/health_checkup_clinic.dart';
+import 'package:estheva_web/website/screens/clinic_services/iv_drips_therapy_clinic.dart';
+import 'package:estheva_web/website/screens/clinic_services/iv_drips_therapy_pacakages_clinic.dart';
+import 'package:estheva_web/website/screens/clinic_services/physiotherapy_at_clinic.dart';
 import 'package:estheva_web/website/screens/home_services/Iv_thearapy_home.dart';
 import 'package:estheva_web/website/screens/home_services/body_coutouring_page_home.dart';
 import 'package:estheva_web/website/screens/home_services/healthcheckup_home.dart';
@@ -59,6 +66,7 @@ class _HeaderBarState extends State<HeaderBar> {
               children: [
                 // Home Services with dropdown submenus
                 _buildHomeServicesMenu(),
+                _buildClinicServiceMenu(),
 
                 _buildMenuItem('Appointments', () {
                   Navigator.push(
@@ -222,7 +230,154 @@ class _HeaderBarState extends State<HeaderBar> {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10.0),
             child: Text(
-              'Home',
+              'Home Care',
+              style: TextStyle(color: white, fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildClinicServiceMenu() {
+    return PopupMenuButton<String>(
+      onSelected: (String result) {
+        // Handle submenu item selection with navigation
+        if (result == 'Body Contouring at Clinic') {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      BodyContouringPageClinic())); // Replace with the actual page for Body Contouring
+        } else if (result == 'IV Drips Therapy at Clinic') {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      IVDripsTherapyClinic())); // Replace with the actual page for Body Contouring
+        } else if (result == 'IV Drips Therapy Packages at Clinic') {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      IVDripsTherapyPackagesClinic())); // Replace with the actual page for Body Contouring
+        } else if (result == 'Health Checkup at Clinic') {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      HealthChecupAtClinic())); // Replace with the actual page for Body Contouring
+        } else if (result == 'Physiotherapy at Clinic') {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      PhysiotherapyAtClinic())); // Replace with the actual page for Body Contouring
+        } else if (result == 'Aesthetic at Clinic') {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      AestheticAtClinic())); // Replace with the actual page for Body Contouring
+        } else if (result == 'Hair Transplant') {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      HairTransplant())); // Replace with the actual page for Body Contouring
+        }
+
+        // Replace with the actual page for Physiotherapy
+      },
+      itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+        PopupMenuItem<String>(
+          value: 'Body Contouring at Clinic',
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => BodyContouringPageClinic()));
+            },
+            child: Text('BODY CONTOURING'),
+          ),
+        ),
+        PopupMenuItem<String>(
+          value: 'IV Drips Therapy at Clinic',
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => IVDripsTherapyClinic()));
+            },
+            child: Text('IV Drips Therapy'),
+          ),
+        ),
+        PopupMenuItem<String>(
+          value: 'IV Drips Therapy Packages at Clinic',
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          IVDripsTherapyPackagesClinic())); // R
+            },
+            child: Text('IV Drips Therapy Packages'),
+          ),
+        ),
+        PopupMenuItem<String>(
+          value: 'Health Checkup at Clinic',
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => HealthChecupAtClinic()));
+            },
+            child: Text('Health CheckUp'),
+          ),
+        ),
+        PopupMenuItem<String>(
+          value: 'Physiotherapy at Clinic',
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => PhysiotherapyAtClinic()));
+            },
+            child: Text('Physiotherapy'),
+          ),
+        ),
+        PopupMenuItem<String>(
+          value: 'Aesthetic at Clinic',
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => AestheticAtClinic()));
+            },
+            child: Text('Aesthetic'),
+          ),
+        ),
+        PopupMenuItem<String>(
+          value: 'Hair Transplant',
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => HairTransplant()));
+            },
+            child: Text('Hair Transplant'),
+          ),
+        ),
+      ],
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Text(
+              'Clinic Services',
               style: TextStyle(color: white, fontSize: 16),
             ),
           ),
