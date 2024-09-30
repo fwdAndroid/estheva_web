27248315d60d3e2b0e0750d668c53069277bc8de
fwdAndroid <fwdkaleem@gmail.com>
2024-09-30 16:08:31 +0500

    fawad

--- lib/website/screens/appointment_web/service_appointment_web.dart
+++ lib/website/screens/appointment_web/service_appointment_web.dart
@@ -14,6 +14,108 @@ class _ServiceAppointmentWebState extends State<ServiceAppointmentWeb> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: HeaderBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+              child: Image.asset(
+            "assets/newlogo.png",
+            height: 200,
+          )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: SizedBox(
+                  width: 250,
+                  height: 250,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (builder) =>
+                                  UpcommingDoctorAppointmentWeb()));
+                    },
+                    child: Card(
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            "assets/logos.png",
+                            height: 170,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("Upcoming Appointment"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: SizedBox(
+                  width: 250,
+                  height: 250,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (builder) =>
+                                  CompleteDoctorAppointmentWeb()));
+                    },
+                    child: Card(
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            "assets/logos.png",
+                            height: 170,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("Complete Appointment"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: SizedBox(
+                  width: 250,
+                  height: 250,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (builder) =>
+                                  CancelDoctorAppointmentWeb()));
+                    },
+                    child: Card(
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            "assets/logos.png",
+                            height: 170,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("Cancel Appointment"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
