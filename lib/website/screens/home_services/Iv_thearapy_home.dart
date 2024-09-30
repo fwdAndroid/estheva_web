a5366163684145390de4c131e9a5df573c55ed30
fwdAndroid <fwdkaleem@gmail.com>
2024-09-30 21:59:35 +0500

    Home Services Section

---
+++ lib/website/screens/home_services/Iv_thearapy_home.dart
@@ -0,0 +1,128 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:estheva_web/uitls/colors.dart';
+import 'package:estheva_web/website/screens/web_details.dart/product_detail_web.dart';
+import 'package:estheva_web/widgets/header_widget.dart';
+import 'package:flutter/material.dart';
+
+class IvThearapyHome extends StatefulWidget {
+  const IvThearapyHome({super.key});
+
+  @override
+  State<IvThearapyHome> createState() => _IvThearapyHomeState();
+}
+
+class _IvThearapyHomeState extends State<IvThearapyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: HeaderBar(),
+      body: StreamBuilder<Object>(
+          stream: FirebaseFirestore.instance
+              .collection("services")
+              .where("serviceCategory", isEqualTo: "IV Drips Therapy")
+              .snapshots(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (!snapshot.hasData || snapshot.data == null) {
+              return Center(child: Text('No Service available'));
+            }
+            var snap = snapshot.data;
+            return GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: _getCrossAxisCount(
+                      context), // Adjust the number of columns based on screen size
+                  crossAxisSpacing: 8.0,
+                  mainAxisSpacing: 8.0,
+                  childAspectRatio: 1.0, // Adjust child aspect ratio as needed
+                ),
+                itemCount: snap.docs.length,
+                itemBuilder: (context, index) {
+                  var serviceData = snap.docs[index].data();
+                  return SizedBox(
+                    width: 200,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (builder) => ProductDetailWeb(
+                                      description:
+                                          serviceData['serviceDescription'],
+                                      discount:
+                                          serviceData['discount'].toString(),
+                                      photoURL: serviceData['photoURL'],
+                                      uuid: serviceData['uuid'],
+                                      price: serviceData['price'].toString(),
+                                      serviceCategory:
+                                          serviceData['serviceCategory'],
+                                      serviceName: serviceData['serviceName'],
+                                      serviceSubCategory:
+                                          serviceData['serviceSubCategory'],
+                                    )));
+                      },
+                      child: Card(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Center(
+                              child: CircleAvatar(
+                                backgroundImage:
+                                    NetworkImage(serviceData['photoURL']),
+                                radius: 60,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 8.0, left: 8, right: 8),
+                              child: Text(
+                                serviceData['serviceSubcategory'],
+                                style: TextStyle(
+                                    color: appColor,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8.0, top: 8),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Color(0xffD3D3D3),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    serviceData['price'].toString() + "AED",
+                                    textAlign: TextAlign.start,
+                                    style: TextStyle(
+                                        color: mainColor,
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                });
+          }),
+    );
+  }
+
+  // Adjust the crossAxisCount based on the screen width
+  int _getCrossAxisCount(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    if (screenWidth > 1200) {
+      return 6; // 6 columns for large screens
+    } else if (screenWidth > 800) {
+      return 4; // 4 columns for medium screens
+    } else {
+      return 2; // 2 columns for small screens
+    }
+  }
+}
--- lib/widgets/header_widget.dart
+++ lib/widgets/header_widget.dart
@@ -1,6 +1,10 @@
-import 'package:estheva_web/screens/main/pages/appointment_page.dart';
 import 'package:estheva_web/uitls/colors.dart';
 import 'package:estheva_web/website/screens/appointment_web/appointment_page_web.dart';
+import 'package:estheva_web/website/screens/home_services/Iv_thearapy_home.dart';
+import 'package:estheva_web/website/screens/home_services/body_coutouring_page_home.dart';
+import 'package:estheva_web/website/screens/home_services/healthcheckup_home.dart';
+import 'package:estheva_web/website/screens/home_services/iv_therapy_packages.dart';
+import 'package:estheva_web/website/screens/home_services/physiotherapy_home.dart';
 import 'package:estheva_web/website/screens/main_dashboard_we.dart';
 import 'package:estheva_web/website/screens/web_dashboard_doctors.dart';
 import 'package:estheva_web/website/screens/web_main_dashboard_account.dart';
@@ -9,7 +13,17 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
+class HeaderBar extends StatefulWidget implements PreferredSizeWidget {
+  @override
+  _HeaderBarState createState() => _HeaderBarState();
+
+  @override
+  Size get preferredSize => Size.fromHeight(60);
+}
+
+class _HeaderBarState extends State<HeaderBar> {
+  bool isHomeServicesExpanded = false; // State to track expansion
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
@@ -19,17 +33,23 @@ class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
       title: Row(
         children: [
           // Logo
-          Row(
-            children: [
-              Image.asset(
-                "assets/logos.png", // Replace with your logo URL
-                height: 40,
-              ),
-              Text(
-                "Medicare",
-                style: GoogleFonts.inter(color: white),
-              )
-            ],
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (builder) => MainDashboardWeb()));
+            },
+            child: Row(
+              children: [
+                Image.asset(
+                  "assets/logos.png", // Replace with your logo URL
+                  height: 40,
+                ),
+                Text(
+                  "Medicare",
+                  style: GoogleFonts.inter(color: white),
+                )
+              ],
+            ),
           ),
           SizedBox(width: 20),
           // Menu items
@@ -37,12 +57,8 @@ class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                _buildMenuItem('Home', () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (builder) => MainDashboardWeb()));
-                }),
+                // Home Services with dropdown submenus
+                _buildHomeServicesMenu(),
 
                 _buildMenuItem('Appointments', () {
                   Navigator.push(
@@ -50,6 +66,7 @@ class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
                       MaterialPageRoute(
                           builder: (builder) => AppointmentPageWeb()));
                 }),
+
                 _buildMenuItem('Doctors', () {
                   Navigator.push(
                       context,
@@ -98,6 +115,119 @@ class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
     );
   }
 
-  @override
-  Size get preferredSize => Size.fromHeight(60);
+  Widget _buildHomeServicesMenu() {
+    return PopupMenuButton<String>(
+      onSelected: (String result) {
+        // Handle submenu item selection with navigation
+        if (result == 'Body Contouring at Home') {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      BodyContouringPageHome())); // Replace with the actual page for Body Contouring
+        } else if (result == 'IV Drips Therapy at Home') {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      IvThearapyHome())); // Replace with the actual page for IV Drips Therapy
+        } else if (result == 'IV Drips Therapy Packages') {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      IVDripsTherapyPackages())); // Replace with the actual page for Physiotherapy
+        } else if (result == 'Health Tests at Home') {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      HealthCheckUpHome())); // Replace with the actual page for Health Tests
+        } else if (result == 'Physiotherapy') {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      PhysiotherapyHome())); // Replace with the actual page for Physiotherapy
+        }
+      },
+      itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+        PopupMenuItem<String>(
+          value: 'Body Contouring at Home',
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => BodyContouringPageHome()));
+            },
+            child: Text('Body Contouring at Home'),
+          ),
+        ),
+        PopupMenuItem<String>(
+          value: 'IV Drips Therapy at Home',
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          IvThearapyHome())); // Navigate to IV Drips Therapy
+            },
+            child: Text('IV Drips Therapy at Home'),
+          ),
+        ),
+        PopupMenuItem<String>(
+          value: 'IV Drips Therapy Packages',
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          IVDripsTherapyPackages())); // Navigate to Physiotherapy
+            },
+            child: Text('IV Drips Therapy Packages'),
+          ),
+        ),
+        PopupMenuItem<String>(
+          value: 'Health Tests at Home',
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          HealthCheckUpHome())); // Navigate to Health Tests
+            },
+            child: Text('Health Tests at Home'),
+          ),
+        ),
+        PopupMenuItem<String>(
+          value: 'Physiotherapy',
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          PhysiotherapyHome())); // Navigate to Physiotherapy
+            },
+            child: Text('Physiotherapy Test at Home'),
+          ),
+        ),
+      ],
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Text(
+              'Home',
+              style: TextStyle(color: white, fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
