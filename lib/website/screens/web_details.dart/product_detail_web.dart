aa3143bada22a83957dd264f9b39d7fde67aecce
fwdAndroid <fwdkaleem@gmail.com>
2024-10-10 18:33:55 +0500

    Flutter

--- lib/website/screens/web_details.dart/product_detail_web.dart
+++ lib/website/screens/web_details.dart/product_detail_web.dart
@@ -32,94 +32,25 @@ class _ProductDetailWebState extends State<ProductDetailWeb> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                FormSection(
-                  uuid: widget.uuid,
-                  serviceSubCategory: widget.serviceSubCategory,
-                  serviceName: widget.serviceName,
-                  serviceCategory: widget.serviceCategory,
-                  discount: widget.discount.toString(),
-                  description: widget.description,
-                  photoURL: widget.photoURL,
-                  price: widget.price.toString(),
-                ),
-                ImageSelection(),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ImageSelection extends StatelessWidget {
-  const ImageSelection({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: Image.asset(
-              "assets/logos.png",
-              height: 300,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class FormSection extends StatelessWidget {
-  final description;
-  final discount;
-  final photoURL;
-  final uuid;
-  final price;
-  final serviceCategory;
-  final serviceName;
-  final serviceSubCategory;
-  const FormSection(
-      {super.key,
-      required this.description,
-      required this.discount,
-      required this.photoURL,
-      required this.price,
-      required this.serviceCategory,
-      required this.serviceName,
-      required this.serviceSubCategory,
-      required this.uuid});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 448,
-      padding: const EdgeInsets.symmetric(horizontal: 50),
-      child: SingleChildScrollView(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image.network(
-              photoURL,
-              height: 200,
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.cover,
-              filterQuality: FilterQuality.high,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.network(
+                widget.photoURL,
+                height: 170,
+                width: 500,
+                fit: BoxFit.cover,
+                filterQuality: FilterQuality.high,
+              ),
             ),
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Text(
-                serviceName,
+                widget.serviceName,
                 style: GoogleFonts.poppins(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
@@ -127,62 +58,73 @@ class FormSection extends StatelessWidget {
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                description,
-                style: GoogleFonts.poppins(
-                  fontSize: 14,
-                  color: Colors.black54,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Container(
-                color: Colors.yellow.shade100,
-                padding: EdgeInsets.all(16),
-                child: Row(
-                  children: [
-                    Icon(Icons.discount, color: Colors.orange),
-                    SizedBox(width: 8),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          discount.toString(),
+            SizedBox(
+                height: 300,
+                width: 1200,
+                child: Scrollbar(
+                    // Add a scrollbar for the description
+                    thumbVisibility: true, // Keep the scrollbar visible
+                    child: SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          widget.description,
                           style: GoogleFonts.poppins(
                             fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),
-                        Text(
-                          "Use code SUMMER20 | above AED 10",
-                          style: GoogleFonts.poppins(
-                            fontSize: 12,
                             color: Colors.black54,
                           ),
                         ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
+                      ),
+                    ))),
+            // Padding(
+            //   padding: const EdgeInsets.all(16.0),
+            //   child: Container(
+            //     color: Colors.yellow.shade100,
+            //     padding: EdgeInsets.all(16),
+            //     child: Row(
+            //       children: [
+            //         Icon(Icons.discount, color: Colors.orange),
+            //         SizedBox(width: 8),
+            //         Column(
+            //           crossAxisAlignment: CrossAxisAlignment.start,
+            //           children: [
+            //             Text(
+            //               widget.discount.toString(),
+            //               style: GoogleFonts.poppins(
+            //                 fontSize: 14,
+            //                 fontWeight: FontWeight.bold,
+            //                 color: Colors.black,
+            //               ),
+            //             ),
+            //             Text(
+            //               "Use code SUMMER20 | above AED 10",
+            //               style: GoogleFonts.poppins(
+            //                 fontSize: 12,
+            //                 color: Colors.black54,
+            //               ),
+            //             ),
+            //           ],
+            //         ),
+            //       ],
+            //     ),
+            //   ),
+            // ),
             Container(
-              color: Colors.grey.shade200,
+              color: Colors.white,
               padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    price.toString() + "AED",
+                    widget.price.toString() + "AED",
                     style: GoogleFonts.poppins(
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
                       color: mainColor,
                     ),
+                  ),
+                  const SizedBox(
+                    width: 20,
                   ),
                   ElevatedButton(
                     onPressed: () {
@@ -191,14 +133,16 @@ class FormSection extends StatelessWidget {
                           context,
                           MaterialPageRoute(
                               builder: (builder) => ProductBookingWeb(
-                                    description: description,
-                                    discount: discount.toString(),
-                                    price: price.toString(),
-                                    serviceCategory: serviceCategory.toString(),
-                                    uuid: uuid,
-                                    serviceName: serviceName,
-                                    serviceSubCategory: serviceSubCategory,
-                                    photoURL: photoURL,
+                                    description: widget.description,
+                                    discount: widget.discount.toString(),
+                                    price: widget.price.toString(),
+                                    serviceCategory:
+                                        widget.serviceCategory.toString(),
+                                    uuid: widget.uuid,
+                                    serviceName: widget.serviceName,
+                                    serviceSubCategory:
+                                        widget.serviceSubCategory,
+                                    photoURL: widget.photoURL,
                                   )));
                     },
                     style: ElevatedButton.styleFrom(
